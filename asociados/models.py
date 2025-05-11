16d27dd89b9c795494b514963aff62b571632041
Juan Díaz <jchendiaz15@gmail.com>
2025-05-11 15:48:41 -0600

    Cambios en la tabla de asociados

--- asociados/models.py
+++ asociados/models.py
@@ -8,6 +8,7 @@ class Asociado(models.Model):
     nombre = models.CharField(max_length=100)
     saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     pin = models.CharField(max_length=10)
+    fecha_registro = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.nombre} ({self.numero_cuenta})"
